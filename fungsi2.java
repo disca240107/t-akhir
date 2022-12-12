a52cdf1f981e7b057c0cbc063bcfbfff8bb43c83
disca240107 <110644406+disca240107@users.noreply.github.com>
2022-12-12 12:06:05 +0700

    Create fungsi2.java

---
+++ fungsi2.java
@@ -0,0 +1,14 @@
+ Scanner input = new Scanner(System.in);
+        int a, b, c;
+
+        System.out.print("Masukan nilai a : ");
+        a = input.nextInt();
+
+        System.out.print("Masukan nilai b : ");
+        b = input.nextInt();
+
+        System.out.print("Masukan nilai c: ");
+        c = input.nextInt();
+        max(a, b, c);
+    }
+}
