2c950a843b934328517495403368323b578fc8e0
disca240107 <110644406+disca240107@users.noreply.github.com>
2022-12-12 12:05:09 +0700

    Create fungsi1.java

---
+++ fungsi1.java
@@ -0,0 +1,17 @@
+public static void hitung(int a, int b) {
+        System.out.println(a + b);
+        
+    }
+
+    public static void hitung2() {
+        System.out.println("nilai a dan b adalah");
+
+    }
+
+    public static void main(String[] args) {
+        int a = 10, b = 2;
+        hitung2();
+        hitung(a, b);
+        
+    }
+}
